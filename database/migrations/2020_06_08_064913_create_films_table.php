f60fe26aa2d63f61afc22aa860331e29977ce884
theanik <anwar.anik33@gmail.com>
2020-06-08 13:05:18 +0600

    Fate : film-modal,controller,migration,seeder

---
+++ database/migrations/2020_06_08_064913_create_films_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->date('release_date');
+            $table->integer('rating');
+            $table->integer('rating_pepole_count')->nullable();
+            $table->double('ticket_price', 15, 8);
+            $table->string('country');
+            $table->string('genre');
+            $table->text('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
