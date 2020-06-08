7e922acceaacb4d9289ee56dd10bf28fc0f768db
theanik <anwar.anik33@gmail.com>
2020-06-08 13:10:01 +0600

    update film migration

--- database/migrations/2020_06_08_064913_create_films_table.php
+++ database/migrations/2020_06_08_064913_create_films_table.php
@@ -16,6 +16,7 @@ class CreateFilmsTable extends Migration
         Schema::create('films', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->text('description');
             $table->date('release_date');
             $table->integer('rating');
